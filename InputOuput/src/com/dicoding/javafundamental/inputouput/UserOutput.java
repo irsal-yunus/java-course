692b8d8cc1e8f62347c79c5418ef3fcd714b5154
irsal.yunus04 <irsal.yunus04@gmail.com>
2021-04-05 15:54:32 +0700

    add module operan and operator

--- InputOuput/src/com/dicoding/javafundamental/inputouput/UserOutput.java
+++ InputOuput/src/com/dicoding/javafundamental/inputouput/UserOutput.java
@@ -6,5 +6,22 @@ public class UserOutput {
         System.out.println("Ini juga dicetak pakai println()");
         System.out.print("Ini dicetak dengan print()");
         System.out.print(" dan ini juga dicetak dengan print()");
+        // Type data string
+        String greeting = "Hello World!";
+
+        // Type data array character
+        char[] dicodingChars = { 'd', 'i', 'c', 'o', 'd', 'i', 'n', 'g' };
+        String dicodingString = new String(dicodingChars);
+        System.out.println(dicodingString);
+
+        // Mengetahui Panjang String
+        String dicoding = "dicoding";
+        int length = dicoding.length();
+        System.out.println(length);
+
+        // Mengambil Karakter Dari Sebuah String
+
+        char result = dicoding.charAt(7);
+        System.out.println(result);
     }
 }
